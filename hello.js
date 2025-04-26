eb91bc4ccae5fb8a469703ce54863ba61fb7aa49
Kingsley <agukingsley450@gmail.com>
2025-04-26 01:48:58 +0100

    Updating hello.js

--- hello.js
+++ hello.js
@@ -4,3 +4,15 @@ console.log("I just created a but need to remove it.");
 console.log("This is a test file.");
 console.log("I just created a but need to remove it.");
 console.log("This is a test file.");
+
+let x = 24;
+let y = 42;
+
+if (x > y) {
+  console.log("x is greater than y");
+}
+else if (x < y) {
+  console.log("x is less than y");
+} else {
+  console.log("x is equal to y");
+}   
