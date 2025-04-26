dfd6ab4e973dab11824f9d2579e1e897a643337d
Kingsley <agukingsley450@gmail.com>
2025-04-26 11:44:09 +0100

    Trying out reverts

--- hello.js
+++ hello.js
@@ -1,21 +1,2 @@
-console.log("Hello, world!");
-console.log("This is a test file.");
-console.log("I just created a but need to remove it.");
-console.log("This is a test file.");
-console.log("I just created a but need to remove it.");
-console.log("This is a test file.");
-
-let x = 24;
-let y = 42;
-
-if (x > y) {
-  console.log("x is greater than y");
-}
-else if (x < y) {
-  console.log("x is less than y");
-} else {
-  console.log("x is equal to y");
-}   
-
-console.log ("This is going very smoothly.");
-console.log("This is a test file.");
+console.log("Trying out reverts");
+console.log("Lets see how this plays out");
